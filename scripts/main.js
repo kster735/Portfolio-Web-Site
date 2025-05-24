ef586c778976e2210b5dfab52d86b724dc993627
Stergios Katsogiannis <kster735@gmial.com>
2025-05-25 00:31:07 +0300

    Fixed hamburger button

--- scripts/main.js
+++ scripts/main.js
@@ -5,7 +5,11 @@ let skillspage = document.querySelector(".skills");
 let projectspage = document.querySelector(".projects-container");
 let certificationspage = document.querySelector(".certifications-container");
 
-const views = [homepage, cvpage, skillspage, projectspage, certificationspage]; 
+const views = [homepage, cvpage, skillspage, projectspage, certificationspage];
+
+const toggle_menu = document.getElementById('toggle-menu');
+const menu = document.querySelector('.menu');
+
 
 views.forEach(v =>{
     v.classList.remove('show');
@@ -15,7 +19,7 @@ let href = sessionStorage.getItem("currentPage");
 
 switch(href){
     case '#home':
-        views[0].classList.add('show'); 
+        views[0].classList.add('show');
         break;
     case '#cv':
         views[1].classList.add('show');
@@ -35,11 +39,17 @@ switch(href){
 }
 
 for (let i=0; i<links.length; i++){
-    links[i].addEventListener('click', (event) =>{
+    links[i].addEventListener('click', (event) => {
+
+        if (toggle_menu.style.display != 'none') {
+            toggle_menu.checked = false;
+            menu.style.display = "none";
+        }
+
         const target = event.target;
         href = target.getAttribute('href');
         console.log(href);
-        sessionStorage.setItem("currentPage", href); 
+        sessionStorage.setItem("currentPage", href);
         for ( let j=0; j<views.length; j++){
             if (j===i){
                 views[j].classList.add('show');
@@ -51,6 +61,32 @@ for (let i=0; i<links.length; i++){
 };
 
 
+
+
+
+toggle_menu.addEventListener('change', (e) => {
+    if (e.target.checked) {
+        menu.style.display = 'flex';
+    } else {
+        menu.style.display = 'none';
+    }
+});
+
+
+window.addEventListener("resize", () => {
+    const width = window.innerWidth;
+    const height = window.innerHeight;
+
+    if (width > 968) {
+        toggle_menu.checked = true;
+        toggle_menu.style.display = 'none';
+        menu.style.display = 'flex';
+    } else {
+        toggle_menu.checked = false;
+        toggle_menu.style.display = 'flex';
+        menu.style.display = 'none';
+    }
+});
 
 
 
